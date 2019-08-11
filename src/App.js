f27062012fafe08a96532d4db64edf40560955e1
Yann Amsellem <yann_ams@icloud.com>
2019-08-11 12:49:30 +0200

    feat: masonry implementation with react-spring

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-shadow */
+import React, { useState, useEffect } from 'react'
+import { animated, useTransition } from 'react-spring'
+import shuffle from 'lodash.shuffle'
+
+import './App.css'
+
+import useMedia from './useMedia'
+import useMeasure from './useMeasure'
+
+const data = Array.from({ length: 15 }, (_, i) => ({ key: i + 1 }))
 
 function App() {
+  const columns = useMedia(
+    ['(min-width: 1500px)', '(min-width: 1000px)', '(min-width: 600px)'],
+    [5, 4, 3],
+    2,
+  )
+
+  const [{ ref }, { width }] = useMeasure()
+
+  const [items, set] = useState(data)
+
+  useEffect(() => {
+    setTimeout(() => set(shuffle), 2000)
+  })
+
+  const heights = new Array(columns).fill(0)
+
+  const gridItems = items.map((child) => {
+    const column = heights.indexOf(Math.min(...heights))
+    const xy = [(width / columns) * column, heights[column]]
+    heights[column] += width / columns
+    return {
+      ...child,
+      xy,
+      width: width / columns,
+      height: width / columns,
+    }
+  })
+
+  const transitions = useTransition(gridItems, item => item.key, {
+    from: ({ xy, width, height }) => ({
+      xy,
+      width,
+      height,
+      opacity: 0,
+    }),
+    enter: ({ xy, width, height }) => ({
+      xy,
+      width,
+      height,
+      opacity: 1,
+    }),
+    update: ({ xy, width, height }) => ({ xy, width, height }),
+    leave: { height: 0, opacity: 0 },
+    config: { mass: 5, tension: 500, friction: 100 },
+    trail: 25,
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div ref={ref} className="list" style={{ height: Math.max(...heights) }}>
+      {transitions.map(({ item, props: { xy, ...style }, key }) => (
+        <animated.div
+          key={key}
+          style={{
+            ...style,
+            transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`),
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div>{item.key}</div>
+        </animated.div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/useMedia.js
@@ -0,0 +1,17 @@
+import { useEffect, useState, useCallback } from 'react'
+
+export default function useMedia(queries, values, defaultValue) {
+  const match = useCallback(
+    () => values[queries.findIndex(q => matchMedia(q).matches)] || defaultValue,
+    [defaultValue, queries, values],
+  )
+
+  const [value, set] = useState(match)
+
+  useEffect(() => {
+    const handler = () => set(match)
+    window.addEventListener('resize', handler)
+    return () => window.removeEventListener('resize', handler)
+  }, [match])
+  return value
+}
